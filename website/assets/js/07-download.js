3bdc76f192cf667be68f591779e61b50a3e59c2c
Simon Baird <sbaird@redhat.com>
2026-01-26 15:43:16 -0500

    Rework the download all button and related js
    - Make it look the same as the other buttons
    - Refactor so it is unhidden by the platform/os detection js
      instead of created from scratch.

--- website/assets/js/07-download.js
+++ website/assets/js/07-download.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const download = document.querySelector('#download a');
+    const downloadAll = document.querySelector('#all-downloads a');
     if (!download) {
         return
     }
     try {
-        const response = await fetch('https://api.github.com/repos/conforma/cli/releases/tags/snapshot');
+        const response = await fetch('https://api.github.com/repos/conforma/cli/releases/latest');
         const snapshot = await response.json();
         const uap = new UAParser();
         const os = uap.getOS().name.replace('macOS', 'darwin').toLowerCase();
@@ -12,11 +13,13 @@ document.addEventListener('DOMContentLoaded', async () => {
         const file = `ec_${os}_${arch}`;
         const asset = snapshot.assets.find(a => a.name === file);
         if (asset) {
-            download.parentNode.insertAdjacentHTML('afterend', `<a class="all-downloads" href=${download.href}>(All downloads)</a>`)
+            // Update the download button href to the user's specific platform/arch
             download.href = asset.browser_download_url;
             download.title = `${uap.getOS().name} / ${uap.getCPU().architecture}`;
+            // Unhide the download all button
+            downloadAll.style.display = 'inline-flex';
         }
-    } catch(e) {
+    } catch (e) {
         // Unable to fetch the releases leave the download link as is
         return;
     }
--- website/content/_index.md
+++ website/content/_index.md
@@ -1,7 +1,8 @@
 ---
 kind: home
 ---
+### [{{< icon "arrow-down-circle" >}} Download](https://github.com/conforma/cli/releases/latest){#download .button}
 
-### [{{< icon "arrow-down-circle" >}} Download](https://github.com/conforma/cli/releases/tag/snapshot){#download .button}
+### [{{< icon "arrow-down-circle" >}} All downloads](https://github.com/conforma/cli/releases/latest){#all-downloads .button}
 
 ### [{{< icon "book-open" >}} Getting Started](docs/user-guide/hitchhikers-guide.html){.button}
